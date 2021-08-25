9e4ba34bbdb6650b446628d686611e5dc4d84360
ian <>
2021-08-25 13:42:13 -0500

    Update static site contents
    Only build and publish the following:
    
    * Compute Edition 21.04
    * Enterprise Edition 21.04
    * Government
    * Troubleshooting
    
    For everything else, link to docs.paloaltonetworks.com

--- build_site.sh
+++ build_site.sh
@@ -5,22 +5,6 @@
 
 declare -r latest="compute_edition_21_04"
 
-show_help() {
-  echo "
-Usage: build_site.sh [OPTIONS] [DOC_SOURCE]
-
-build_site.sh builds the Prisma Cloud Compute static site.
-
-OPTIONS:
-  -r                            Populate release notes with CDN download links
-
-
-DOC_SOURCE:
-  path                          Path to AsciiDoctor doc source from the twistlock/docs repo
-                                (default: current working directory)
-"
-}
-
 clear_output_dir() {
   # Delete all files in the output dir, except the .git dir.
   glob="$output_dir""/*"
@@ -29,20 +13,6 @@ clear_output_dir() {
     rm -r "$p"
   done
 }
-
-optspec=":r"
-while getopts "${optspec}" opt; do
-  case "${opt}" in
-    r )
-      publish_cdn_links=true
-      ;;
-    \?)
-      show_help
-      exit
-      ;;
-  esac
-done
-shift "$((OPTIND-1))"
 
 echo "Building static site..."
 if [ "$1" != "" ]; then
@@ -55,10 +25,6 @@ work_dir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 output_dir="$work_dir""/output"
 
 srcAdmin="$doc_dir""/admin_guide"
-srcRN="$doc_dir""/rn"
-srcOps="$doc_dir""/ops_guide"
-srcRefArch="$doc_dir""/ref_arch"
-srcHistorical="$doc_dir""/historical"
 srcTroubleshooting="$doc_dir""/troubleshooting"
 srcGovernment="$doc_dir""/government"
 
@@ -143,109 +109,6 @@ cd "$output_dir"
 git add -A
 git commit -q -m "Commit admin guide (SaaS)"
 
-#
-# RELEASE NOTES
-#
-
-# Create a branch.
-git checkout -b rn
-
-# Delete all files.
-clear_output_dir
-
-# Copy files into place.
-echo "Copy release notes files"
-cd "$work_dir"
-cp -R "$work_dir""/_files/." "$output_dir"
-cp -R "$srcRN""/." "$output_dir"
-mv "$output_dir""/_topic_map_static_site.yml" "$output_dir""/_topic_map.yml"
-
-# Fix adoc source files
-python "_build/format_fixup.py" "$output_dir""/_topic_map.yml"
-if [ "$publish_cdn_links" == "true" ]; then
-  python "_build/rn_details.py" "$output_dir""/_topic_map.yml" "../docs-tools/release_info.yml"
-fi
-
-# Commit files.
-echo "Commit release files"
-cd "$output_dir"
-git add -A
-git commit -q -m "Commit release notes"
-
-#
-# OPS GUIDE
-#
-
-# Create a branch.
-git checkout -b ops
-
-# Delete all files.
-clear_output_dir
-
-# Copy files into place.
-echo "Copy Ops Guide files"
-cd "$work_dir"
-cp -R "$work_dir""/_files/." "$output_dir"
-cp -R "$srcOps""/." "$output_dir"
-
-# Fix adoc source files
-python "_build/format_fixup.py" "$output_dir""/_topic_map.yml"
-
-# Commit files.
-echo "Commit Ops Guide files"
-cd "$output_dir"
-git add -A
-git commit -q -m "Commit Ops Guide"
-
-#
-# REFERENCE ARCHITECTURE
-#
-
-# Create a branch.
-git checkout -b ref_arch
-
-# Delete all files.
-clear_output_dir
-
-# Copy files into place.
-echo "Copy Ref Arch files"
-cd "$work_dir"
-cp -R "$work_dir""/_files/." "$output_dir"
-cp -R "$srcRefArch""/." "$output_dir"
-
-# Fix adoc source files
-python "_build/format_fixup.py" "$output_dir""/_topic_map.yml"
-
-# Commit files.
-echo "Commit Ref Arch files"
-cd "$output_dir"
-git add -A
-git commit -q -m "Commit Ref Arch"
-
-#
-# HISTORICAL
-#
-
-# Create a branch.
-git checkout -b historical
-
-# Delete all files.
-clear_output_dir
-
-# Copy files into place.
-echo "Copy Historical files"
-cd "$work_dir"
-cp -R "$work_dir""/_files/." "$output_dir"
-cp -R "$srcHistorical""/." "$output_dir"
-
-# Fix adoc source files
-python "_build/format_fixup.py" "$output_dir""/_topic_map.yml"
-
-# Commit files.
-echo "Commit Historical files"
-cd "$output_dir"
-git add -A
-git commit -q -m "Commit Historical"
 
 #
 # TROUBLESHOOTING
